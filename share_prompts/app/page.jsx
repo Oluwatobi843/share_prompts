caff9e79c11693011dffd42dad5fd3377d47a6cd
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-07-12 23:27:34 +0100

    Update page.jsx

--- share_prompts/app/page.jsx
+++ share_prompts/app/page.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1>
-        Discover & Sharee
+        Discover & Share
       </h1>
     </section>
 
